2e686dd24ce78b62a0dace8ca610950059605f5e
Shrushti <99087640+shrushtivachhani@users.noreply.github.com>
2025-01-20 14:25:13 +0530

    update pro6

--- PYTHON/PRO6Histogram.py
+++ PYTHON/PRO6Histogram.py
@@ -2,5 +2,16 @@ def histogram(values):
     for value in values:
         print('*' * value)
 
+numbers = input()
+numbers = [int(x) for x in numbers.split()]
+histogram(numbers)
 
-histogram([4, 9, 7])
+'''
+
+print("Enter the numbers separated by space")
+numbers = input()
+numbers = [int(x) for x in numbers.split()]
+for i in range(len(numbers)):
+    print("*" * numbers[i])
+
+'''
